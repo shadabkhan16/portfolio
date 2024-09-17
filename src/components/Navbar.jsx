12f487b3631644c565983b1feaa8dc567576505f
shadabkhan16 <shadab.khan@quadbtech.com>
2024-09-17 15:44:14 +0530

    navbar implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,24 @@
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="bg-black text-white  px-8  md:px-16 lg:px-24">
+            <div className=" container py-2  flex  justify-center md:justify-between items-center">
+                <div className="text-2xl  font-bold  hidden md:inline">Shadab Khan</div>
+                <div className="space-x-6">
+                    <a href="#home" className="hover:text-gray-400">Home</a>
+
+                    <a href="#service" className="hover:text-gray-400">Services</a>
+                    <a href="#projects" className="hover:text-gray-400">Projects</a>
+                    <a href="#about" className="hover:text-gray-400">About</a>
+                    <a href="#contact" className="hover:text-gray-400">Contact</a>
+
+                </div>
+                <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform 
+                 duration-500 hover:scale-100 px-4 py-2 rounded-full">Connect Me</button>
+
+            </div>
+
+        </nav>
     )
 }
 

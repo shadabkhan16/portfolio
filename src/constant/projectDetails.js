8eca89ba8d47f7ba4844a96551315093f7694d56
shadabkhan16 <shadab.khan@quadbtech.com>
2024-09-17 16:47:58 +0530

    project details added  and utilised it

---
+++ src/constant/projectDetails.js
@@ -0,0 +1,29 @@
+import project1 from "../assets/project1.jpg";
+import project2 from "../assets/project2.jpg";
+
+export const projects = [
+  {
+    id: 1,
+    name: "E-cart",
+    technologies: "MERN Stack",
+    image: project1,
+  },
+  {
+    id: 2,
+    name: "Admin Login",
+    technologies: "React Js + Firebase",
+    image: project2,
+  },
+  {
+    id: 3,
+    name: "E-cart",
+    technologies: "React Js + AWS",
+    image: project1,
+  },
+  {
+    id: 4,
+    name: "Admin Login",
+    technologies: "React Js ",
+    image: project2,
+  },
+];

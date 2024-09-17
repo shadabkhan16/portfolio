ce12940396440dc73b6b59907523054972eb9d81
shadabkhan16 <shadab.khan@quadbtech.com>
2024-09-17 16:26:38 +0530

    hero-section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import demo from "../assets/demo.jpg"
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className="bg-black text-white  text-center  py-16">
+            <img src={demo} alt="" className="mx-auto mb-8 w-48 h-48 rounded-full object-cover transform  transition-transform duration-300  hover:scale-105" />
+            <h1 className="text-4xl font-bold"> I'm {" "}
+                <span className="text-transparent  bg-clip-text  bg-gradient-to-r from-green-400 to- blue-400">Shadab Khan
+                </span>
+                ,Front-End Developer
+            </h1>
+            <p className="mt-4 text-lg  text-gray-300">
+                I  specialize in bulding modern  and responsive web applications.
+            </p>
+            <div className="mt-8 space-x-4">
+                <button className="bg-gradient-to-r from-green-400  to-blue-500 text-white  transform transition-transform  duration-300  hover:scale-105  px-4 py-2 rounded-full">
+                    Contact With Me
+                </button>
+            </div>
+        </div>
     )
 }
 

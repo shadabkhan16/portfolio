b5cd1ae988612eefc78179ff3cd8b54c38f2813b
shadabkhan16 <shadab.khan@quadbtech.com>
2024-09-17 15:30:33 +0530

    importing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Projects from "./components/Projects"
+import Services from "./components/Services"
+import About from "./components/About"
+import Footer from "./components/Footer"
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Services />
+      <About />
+      <Footer />
+    </div>
   )
 }
 
